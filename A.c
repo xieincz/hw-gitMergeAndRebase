920938d7d9cccfebc4435805bea2097cf22d8e97
Teng Zhang <zhangt2333@gmail.com>
2021-05-14 22:04:41 +0800

    G: finish feature A,B,C

--- A.c
+++ A.c
@@ -1,4 +1,4 @@
 #include <stdio.h>
 int main() {
-    printf("A"); // A
+    printf("A: a good feature"); // A
 }
--- B.c
+++ B.c
@@ -1,4 +1,4 @@
 #include <stdio.h>
 int main() {
-    printf("B"); // B
+    printf("B: a good feature"); // B
 }
